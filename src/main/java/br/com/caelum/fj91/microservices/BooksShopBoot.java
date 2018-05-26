9533a1996dfad19dabaabd36e8750b3860edb48e
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-05-25 23:01:56 -0300

    Improving Circuit breaker

--- src/main/java/br/com/caelum/fj91/microservices/BooksShopBoot.java
+++ src/main/java/br/com/caelum/fj91/microservices/BooksShopBoot.java
@@ -3,8 +3,12 @@ package br.com.caelum.fj91.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @EnableCaching
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 @SpringBootApplication
 public class BooksShopBoot {
 
--- src/main/java/br/com/caelum/fj91/microservices/rest/BooksClient.java
+++ src/main/java/br/com/caelum/fj91/microservices/rest/BooksClient.java
@@ -3,15 +3,18 @@ package br.com.caelum.fj91.microservices.rest;
 import br.com.caelum.fj91.microservices.dtos.BookDTO;
 import br.com.caelum.fj91.microservices.exceptions.NotFoundException;
 import br.com.caelum.fj91.microservices.models.Product;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriTemplate;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,6 +24,8 @@ public class BooksClient {
     private RestTemplate rest = new RestTemplate();
     private String baseURI = "http://localhost:8081/books";
 
+    @Cacheable("product")
+    @HystrixCommand(fallbackMethod = "fallbackById")
     public Product getProductById(Long id) {
 
         URI uri = URI.create(baseURI+"/"+id);
@@ -36,7 +41,13 @@ public class BooksClient {
         throw new NotFoundException();
     }
 
+    @CacheEvict(value = "product", allEntries = true)
+    public Product fallbackById(Long id){
+        throw new NotFoundException();
+    }
 
+    @Cacheable("products")
+    @HystrixCommand(fallbackMethod = "fallbackAll")
     public List<Product> getAllProducts() {
 
         URI uri = URI.create(baseURI);
@@ -50,5 +61,10 @@ public class BooksClient {
         }
 
         throw new NotFoundException();
+    }
+
+    @CacheEvict(value = "products", allEntries = true)
+    public List<Product> fallbackAll(){
+        return new ArrayList<>();
     }
 }
